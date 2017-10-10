0f3e88b50aa62f05ac88d335fe69172b7e0407fe
warron <warron@WARRON-2.local>
2017-10-10 16:30:05 +0800

    no message

--- DDFactorya.h
+++ DDFactorya.h
@@ -9,9 +9,6 @@
 #import <Foundation/Foundation.h>
 
 @interface DDFactory : NSObject
-
-+(instancetype)factory;
-
 -(void)success;
 @end
 
--- DDFactorya.m
+++ DDFactorya.m
@@ -10,28 +10,6 @@
 #import "DDFactorya.h"
 
 @implementation DDFactory
-
-+(instancetype)factory{
-    
-    static  DDFactory *factory  = nil;
-    static dispatch_once_t once;
-    if (!factory) {
-        dispatch_once(&once, ^{
-            factory = [[DDFactory alloc]initPrivate];
-         
-        });
-    }
-    return factory;
-}
- 
--(instancetype)init{
-    //不允许用init方法
-    @throw [NSException exceptionWithName:@"Singleton" reason:@"FirstVC is a Singleton, please Use shareView to create" userInfo:nil];
-}
-
--(instancetype)initPrivate{
-    return  self = [super init];
-}
 -(void)success{
     
     NSLog(@"成功");
